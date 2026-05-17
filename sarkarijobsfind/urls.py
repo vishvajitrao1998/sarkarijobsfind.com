aab35adba5754ad5d86ceaf3d28df5d440875f09
vishvajitrao1998 <vishvajitrao1998@gmail.com>
2026-05-17 13:24:13 +0530

    change admin url

--- sarkarijobsfind/urls.py
+++ sarkarijobsfind/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("jobs.urls")),
     path("admin/", admin.site.urls),
+    path("", include("jobs.urls"))
 
 ]
