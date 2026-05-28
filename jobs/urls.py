f48e5c0485902df8437cde3749f5f3d9a7731b51
vishvajitrao1998 <vishvajitrao1998@gmail.com>
2026-05-28 21:10:50 +0530

    added push notification

--- jobs/urls.py
+++ jobs/urls.py
@@ -8,20 +8,6 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
-    path("<str:slug>", job_detail_view, name="job_detail_view"),
-    path("category/<str:slug>", category_jobs, name="category_jobs"),
-
-
-    path("p/privacy-policy", privacy_policy, name="privacy_policy"),
-    path("p/about-us", about_us, name="about_us"),
-    path("p/terms-and-conditions", terms_and_conditions, name="terms_and_conditions"),
-    path("p/contact", contact, name="contact"),
-
-]
-
-
-urlpatterns += [
-
     path(
         "OneSignalSDKWorker.js",
         TemplateView.as_view(
@@ -37,8 +23,19 @@ urlpatterns += [
             content_type="application/javascript",
         ),
     ),
+    path("<str:slug>", job_detail_view, name="job_detail_view"),
+    path("category/<str:slug>", category_jobs, name="category_jobs"),
+
+
+    path("p/privacy-policy", privacy_policy, name="privacy_policy"),
+    path("p/about-us", about_us, name="about_us"),
+    path("p/terms-and-conditions", terms_and_conditions, name="terms_and_conditions"),
+    path("p/contact", contact, name="contact"),
 
 ]
+
+
+
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
